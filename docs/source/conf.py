785ea6e7afd93cb7ee2616a49513ea8c5f19d778
João Fernandes <joao.fernandes@cern.ch>
2018-05-24 16:28:57 +0200

    more changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -79,7 +79,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'default'
-html_theme_options = {'display_version': False}
+html_show_sourcelink = False
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
