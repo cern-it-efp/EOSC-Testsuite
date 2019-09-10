739c03e44ea3837a8ac00c2b3905ccc1a3acfad2
ipelu <ignacio.peluaga.lozada@cern.ch>
2019-09-10 17:38:16 +0200

    Dev: azure

--- templates/azure/rawProvision.tf
+++ templates/azure/rawProvision.tf
@@ -1,3 +1,6 @@
+provider "azurerm" {
+}
+
 resource "azurerm_virtual_network" "myterraformnetwork" {
   name                = "myVnet"
   address_space       = ["10.0.0.0/16"]
@@ -32,8 +35,8 @@ resource "azurerm_network_security_group" "myterraformnsg" {
 }
 
 resource "azurerm_network_interface" "terraformnic" {
-  count                      = var.amount
-  name                      = "myNIC${count.index}" # this needs to have on the name the randomId of the node it will be linked to
+  count                     = "${var.amount}"
+  name                      = "myNIC${count.index}-${var.clusterRandomID}"
   location                  = "${var.location}"
   resource_group_name       = "${var.resourceGroupName}"
   network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
@@ -41,5 +44,46 @@ resource "azurerm_network_interface" "terraformnic" {
     name                          = "myNicConfiguration"
     subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
     private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "kubenode" {
+  count                 = "${var.amount}"
+  name                  = "${var.instanceName}"
+  location              = "${var.location}"
+  resource_group_name   = "${var.resourceGroupName}"
+  network_interface_ids = [element(azurerm_network_interface.terraformnic.*.id, count.index)]
+  storage_os_disk {
+    name              = "myOsDisk${count.index}-${var.clusterRandomID}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+  storage_image_reference {
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7.5"
+    version   = "latest"
+  }
+  os_profile {
+    computer_name  = "${var.instanceName}"
+    admin_username = "${var.openUser}"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      key_data = "${var.pubSSH}"
+      path     = "/home/${var.openUser}/.ssh/authorized_keys"
+    }
+  }
+  provisioner "remote-exec" {
+    connection {
+      host        = element(azurerm_network_interface.terraformnic.*.private_ip_address, count.index)
+      type        = "ssh"
+      user        = "${var.openUser}"
+      private_key = file("~/.ssh/id_rsa")
+      timeout     = "30m"
+    }
+    inline = ["sudo mkdir /root/.ssh ; sudo cp /home/${var.openUser}/.ssh/authorized_keys /root/.ssh"]
   }
 }
