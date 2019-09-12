76a156f12c1d22b9874917dd08396c4c96b00f96
ipelu <ignacio.peluaga.lozada@cern.ch>
2019-09-12 18:27:18 +0200

    Restructure and refactoring

---
+++ src/checker.py
@@ -0,0 +1,8 @@
+def checkCost(value):
+    """ Checks the provided value is not None and is greater than 0.
+
+    Parameters:
+        value: Value to be checked.
+    """
+
+    return value >= 0 and obtainCost is True if value else False
