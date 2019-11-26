1ebbc20ed84158dee770265da345f6b17e95d890
ipelu <ignacio.peluaga.lozada@cern.ch>
2019-11-26 17:46:36 +0100

    Updated tf script to create launcher on AWS: use EU region

--- src/terraform/providers/aws/main_centos.tf
+++ src/terraform/providers/aws/main_centos.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "eu-central-1"
 }
 
 variable "openUser" {
@@ -10,7 +10,9 @@ resource "aws_instance" "launcher" { # With no more code, this VM will use the d
   tags = {
       Name = "TSlauncher"
   }
-  ami = "ami-08ee2516c7709ea48" # "ami-e1496384"
+  #ami = "ami-08ee2516c7709ea48" # Centos US
+  ami = "ami-0957ba512eafd08d9" # Centos EU
+  #ami = "ami-e1496384" # Ubuntu US
   instance_type = "t2.medium"
   key_name = "exo"
   root_block_device {
