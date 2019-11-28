8b004fe589143f4e01d399491868caeb85d8bc1c
ipelu <ignacio.peluaga.lozada@cern.ch>
2019-11-28 15:29:45 +0100

    improved solution to allow root ssh at bootstrap.tf template

--- src/templates/general/bootstrap.tf
+++ src/templates/general/bootstrap.tf
@@ -11,8 +11,8 @@ resource "null_resource" "allow_root" {
     inline = [
       "sudo mkdir /root/.ssh",
       "sudo cp /home/${var.openUser}/.ssh/authorized_keys /root/.ssh",
-      "sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config",
-      "sudo systemctl restart sshd"
+      "sudo sed -i '1iPermitRootLogin yes\n' /etc/ssh/sshd_config",
+      "sudo systemctl restart sshd || sudo service sshd restart"
     ]
   }
 }
