de11fa18a36366dfa2a9572743ccbbec2c48a267
Jakub Urban <jakub@jakubmacbookpro.dyndns.cern.ch>
2019-11-29 18:58:17 +0100

    abspath8

--- src/tests/lib/checker.py
+++ src/tests/lib/checker.py
@@ -4,8 +4,8 @@ import os
 import sys
 
 #jenkins library configuration
-sys.path.append(os.path.abspath(os.environ['WORKSPACE'] + "/src/tests/lib"))
-from aux import *
+sys.path.append(os.path.abspath(os.environ['WORKSPACE'] + "/src/tests"))
+from lib.aux import *
 
 try:
     import jsonschema
