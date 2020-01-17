3326c310befc2e8c2883834ff8e42e9d7ab02150
ipelu <ignacio.peluaga.lozada@cern.ch>
2020-01-17 18:25:13 +0100

    Updated tf templates to comply with version 12.19

--- src/templates/openstack/rawProvision.tf
+++ src/templates/openstack/rawProvision.tf
@@ -2,12 +2,12 @@ provider "openstack" {
 }
 
 resource "openstack_compute_instance_v2" "kubenode" {
-  count                 = "${var.customCount}"
-  flavor_name           = "${var.flavor}"
+  count                 = var.customCount
+  flavor_name           = var.flavor
   name                  = "${var.instanceName}-${count.index}"
-  image_name = "${var.imageName}"
-  key_pair = "${var.keyPair}"
+  image_name = var.imageName
+  key_pair = var.keyPair
   security_groups = var.securityGroups
-  region = "${var.region}" # errors if set to "" ?
-  availability_zone = "${var.availabilityZone}" # errors if set to "" ?
+  region = var.region
+  availability_zone = var.availabilityZone
 }
